0921176a3c64c27dd74f2d08434a6fc146c23bf1
Wouter Koch <wouter.koch@artsdatabanken.no>
2018-02-15 16:07:04 +0100

    Generalize functions, uniform density maps

--- exploration.R
+++ exploration.R
@@ -4,30 +4,54 @@ read_gbif <- function(path) {
 }
 
 read_geostat <- function(path) {
-  result <- read.table(path, sep=",", header=TRUE, quote = "", fill=TRUE)
+  result <- read.table(path, sep=",", header=TRUE, fill=TRUE)
   result
 }
 
-geostat_add_utm <- function(dataset) {
-  dataset$utm_y <- strtoi(substr(dataset$X.GRD_ID, 6, 9)) * 1000
-  dataset$utm_x <- strtoi(substr(dataset$X.GRD_ID, 11, 14)) * 1000
+filter_observations <- function(dataset, filter) {
+  result <- subset(dataset, dataset[,filter[1]] %in% filter[-1] & taxonrank == "SPECIES")
+  result <- droplevels(result)
+  result
+}
+
+rasterize <- function(dataset, km=1) {
+  if(!("utm_x" %in% colnames(dataset))) {
+    dataset <- add_UTM(dataset)
+  }
   
-  xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$utm_x, Y = dataset$utm_y)
+  dataset$utm_x_rounded <- (dataset$utm_x %/% (1000 * km)) * (1000 * km)
+  dataset$utm_y_rounded <- (dataset$utm_y %/% (1000 * km)) * (1000 * km)
+  
+  xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$utm_x_rounded, Y = dataset$utm_y_rounded)
   coordinates(xy) <- c("X", "Y")
   proj4string(xy) <- CRS("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs")
   res <- spTransform(xy, CRS("+proj=longlat +datum=WGS84"))
   res <- as.data.frame(res)
   
-  dataset$long <- res$X
-  dataset$lat <- res$Y
-
+  dataset$long_rounded <- res$X
+  dataset$lat_rounded <- res$Y
+  
   return(dataset)
 }
 
-filter_by_genus <- function(dataset, genera) {
-  result <- subset(dataset, genus %in% genera & taxonrank == "SPECIES")
-  result <- droplevels(result)
-  result
+
+add_UTM <- function(dataset) {
+  if("GRD_ID" %in% colnames(dataset)) {
+    dataset$utm_y <- strtoi(substr(dataset$GRD_ID, 5, 8)) * 1000
+    dataset$utm_x <- strtoi(substr(dataset$GRD_ID, 10, 13)) * 1000
+  }
+  else if("decimallongitude" %in% colnames(dataset)) {
+    xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$decimallongitude, Y = dataset$decimallatitude)
+    coordinates(xy) <- c("X", "Y")
+    proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")
+    res <- spTransform(xy, CRS("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"))
+    res <- as.data.frame(res)
+    
+    dataset$utm_x <- res$X
+    dataset$utm_y <- res$Y
+  }
+
+  return(dataset)
 }
 
 summarize_list <- function(list) {
@@ -36,52 +60,6 @@ summarize_list <- function(list) {
     list <- list[-length(list)]
   }
   paste(list, collapse = ", ")
-}
-
-add_rounded_coordinates <- function(dataset, km=1) {
-  xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$decimallongitude, Y = dataset$decimallatitude)
-  coordinates(xy) <- c("X", "Y")
-  proj4string(xy) <- CRS("+proj=longlat +datum=WGS84")
-  res <- spTransform(xy, CRS("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"))
-  res <- as.data.frame(res)
-  
-  dataset$utm_x <- res$X + 500 * km
-  dataset$utm_y <- res$Y + 500 * km 
-  dataset$utm_x_rounded <- (dataset$utm_x / 1000) %/% km
-  dataset$utm_y_rounded <- (dataset$utm_y / 1000) %/% km
-  
-  xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$utm_x_rounded * 1000 * km, Y = dataset$utm_y_rounded * 1000 * km)
-  coordinates(xy) <- c("X", "Y")
-  proj4string(xy) <- CRS("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs")
-  res <- spTransform(xy, CRS("+proj=longlat +datum=WGS84"))
-  res <- as.data.frame(res)
-  
-  dataset$long_rounded <- res$X
-  dataset$lat_rounded <- res$Y
-
-  return(dataset)
-}
-
-
-
-utm_add_rasterized <- function(dataset, km=1) {
-  dataset$utm_x_rounded <- ((dataset$utm_x + 500 * km) / 1000) %/% km
-  dataset$utm_y_rounded <- ((dataset$utm_y + 500 * km) / 1000) %/% km
-  
-  dataset <- dataset %>% group_by(utm_x_rounded, utm_y_rounded) %>% summarise(pop=sum(X.TOT_P.))
-
-  xy <- data.frame(ID = 1:length(dataset[, 1]), X = dataset$utm_x_rounded * 1000 * km, Y = dataset$utm_y_rounded * 1000 * km)
-  coordinates(xy) <- c("X", "Y")
-  proj4string(xy) <- CRS("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs")
-  res <- spTransform(xy, CRS("+proj=longlat +datum=WGS84"))
-  res <- as.data.frame(res)
-  
-  dataset$long_rounded <- res$X
-  dataset$lat_rounded <- res$Y
-
-  dataset$raster_cell <- paste("x", dataset$utm_x_rounded, "y", dataset$utm_y_rounded)
-
-  return(dataset)
 }
 
 join_by_utm_rounded <- function(observations, environment) {
